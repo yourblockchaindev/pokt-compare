03f6b4972000b0768661a0d657b51d7cb54ec0b7
yourblockchaindev <your.blockchain.dev@gmail.com>
2022-04-18 19:10:43 +0200

    providers

---
+++ src/data/providers/providers.tsx
@@ -0,0 +1,23 @@
+import blockspaces from "./blockspaces.json"
+import c0d3r from "./c0d3r.json"
+import chainflow from "./chainflow.json"
+import chainstack from "./chainstack.json"
+import easy2stake from "./easy2stake.json"
+import liquify from "./liquify.json"
+import nachonodes from "./nachonodes.json"
+import nodefolio from "./nodefolio.json"
+import rivet from "./rivet.json"
+
+const providers = [
+  blockspaces,
+  c0d3r,
+  chainflow,
+  chainstack,
+  easy2stake,
+  liquify,
+  nachonodes,
+  nodefolio,
+  rivet,
+]
+
+export default providers
